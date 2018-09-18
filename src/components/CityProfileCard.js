23f5c815d41848f3fe0331bd351b41fde7c9e437
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-09-17 22:44:33 -0700

    Some css cleanup

--- src/components/CityProfileCard.js
+++ src/components/CityProfileCard.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -11,45 +10,9 @@ import CityStatsCard from '../components/CityStatsCard';
 import Spin from '../components/Spin';
 import data from '../data/data';
 
-const styles = {
-  media: {
-    cursor: 'default',
-    objectFit: 'cover',
-    width: '100%',
+import '../App.css';
 
-  },
-  header: {
-    textAlign: 'center',
-  },
-  headerContainer: {
-    height: '150px',
-    position: 'relative',
-    textAlign: 'center',
-    color: '#ffffff',
-  },
-  cityHeader: {
-    display: 'inline-block',
-  },
-  rankingIcon: {
-    width: '40px',
-    height: '40px',
-    borderRadius: '50%',
-    textAlign: 'center',
-    fontSize: '40px',
-    border: '2px solid #ffffff',
-    display: 'inline-block',
-    marginRight: '10px',
-  },
-  alignIconHeader: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
-
-
-class CityProfileCard extends Component {
+export default class CityProfileCard extends Component {
   state = {
     cityData: null,
   };
@@ -67,7 +30,6 @@ class CityProfileCard extends Component {
 
   render() {
     const { cityData } = this.state;
-    const { classes } = this.props;
 
     if (!cityData) {
       return <Spin/>;
@@ -75,24 +37,24 @@ class CityProfileCard extends Component {
 
     return (
       <div>
-        <div className={classes.headerContainer}>
+        <div className="headerContainer">
           <img src={require('../' + cityData.headerImage)} alt={`${cityData.cityName} Header`}
                style={{ width: '100%', height: '100%' }}/>
-          <div className={classes.alignIconHeader}>
-            <div className={classes.rankingIcon}>{cityData.ranking}</div>
-            <h1 className={classes.cityHeader}>{cityData.cityName}</h1>
+          <div className="alignIconHeader">
+            <div className="rankingIcon">{cityData.ranking}</div>
+            <h1 className="cityHeader">{cityData.cityName}</h1>
           </div>
         </div>
-        <Card className={classes.card} style={{ margin: '10px' }}>
+        <Card style={{ margin: '10px' }}>
           <CardContent>
-            <h3 className={classes.header}>
+            <h3 style={{textAlign: 'center'}}>
               {cityData.cityName} Map Quality
             </h3>
           </CardContent>
           <CardActionArea style={{ width: '100%' }}>
             <CardMedia
               component="img"
-              className={classes.media}
+              className="cardMedia"
               height="323px"
               image={require('../' + cityData.mapImage)}
             />
@@ -103,12 +65,4 @@ class CityProfileCard extends Component {
     );
   }
 }
-
-CityProfileCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(CityProfileCard);
-
-
 
--- src/components/CityStatsCard.js
+++ src/components/CityStatsCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import GridList from '@material-ui/core/GridList';
@@ -8,46 +7,19 @@ import GridListTile from '@material-ui/core/GridListTile';
 
 import OutlinedFlag from '@material-ui/icons/OutlinedFlag';
 
-const styles = {
-  card: {
-    minWidth: 275,
-    borderRadius: 0,
-  },
-  cardHeader: {
-    textAlign: 'center',
-    borderBottom: '1px solid rgba(151,151,151, 0.27)',
-    padding: '4px 0 20px 0',
-  },
-  title: {
-    marginBottom: 16,
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-  },
-  gridList: {
-    width: 500,
-    border: '1px center #979797 100%'
-  },
-};
+import '../App.css';
 
 const CityStatsCard = props => {
-  const { classes, data } = props;
+  const { data } = props;
   return (
-    <Card className={classes.card} style={{ margin: '10px' }}>
+    <Card className="statsCard" style={{ margin: '10px' }}>
       <CardContent>
-        <h3 className={classes.cardHeader}>
+        <h3 className="cardHeader">
           Quick Stats
         </h3>
 
-        <div className={classes.root}>
-          <GridList cellHeight={160} className={classes.gridList} cols={2}>
+        <div className="gridRoot">
+          <GridList cellHeight={160} className="gridList" cols={2}>
             <GridListTile key={data.features} cols={1}>
               <h4>{data.features}</h4>
               <p>Number of OSM Features</p>
@@ -73,8 +45,7 @@ const CityStatsCard = props => {
 };
 
 CityStatsCard.propTypes = {
-  classes: PropTypes.object.isRequired,
   data: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(CityStatsCard);
+export default CityStatsCard;
