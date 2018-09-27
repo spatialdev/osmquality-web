deea4b3c9339507a81e8af4abea5a508b825ac75
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-09-27 09:06:05 -0700

    Added logic to accommodate single page app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <Router basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App/>
-  </Router>
+  </BrowserRouter>
 ), document.getElementById('root'));
 registerServiceWorker();
