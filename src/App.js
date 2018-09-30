a249534c0af4e1b10991354209692cbe1850a8ff
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-09-29 17:37:30 -0700

    Add div to make sure that Open Sans is loaded when DOM is rendered

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
+
+  //Note that we have the fontFamily div to make sure that the font is loaded when the DOM is rendered. This is important
+  // for rendering the ranking icon on the CityProfileCards
   render() {
     return (
       <div style={{ position: 'relative', minHeight: '100vh' }}>
-        {window.location.pathname !== '/' ? <Header/>: null}
+        {window.location.pathname !== '/' ? <Header/> : null}
         <Main/>
-        {window.location.pathname !== '/' ?  <Footer/> : null}
+        {window.location.pathname !== '/' ? <Footer/> : null}
+        <div style={{ fontFamily: 'Open Sans' }}/>
       </div>
     );
   }
