0edb73bff757e0be4cbd642bb78f6916289dda5a
Cole Bessee <cole.bessee@critigen.com>
2018-10-02 09:08:56 -0700

    Some styling and layout updates for methodology content

--- src/components/Methodology.js
+++ src/components/Methodology.js
@@ -13,8 +13,15 @@ import ListItem from '@material-ui/core/es/ListItem/ListItem';
 const styles = () => ({
   root: {
     margin: '10px',
-    display: 'inline-flex',
+    width: 'calc(100% - 20px)',
     alignItems: 'center',
+  },
+
+  wrapper: {
+    margin: '10px 10px 0px 10px',
+    width: 'calc(100% - 20px)',
+    alignItems: 'center',
+    paddingBottom: '100px'
   }
 });
 
@@ -23,9 +30,9 @@ const Methodology = props => {
   const { classes } = props;
 
   return (
-    <Card className={classes.root}>
-      <CardContent style={{ padding: '15px 60px' }}>
-        <h3 style={{ textAlign: 'center' }}>Methodology and Data Page</h3>
+    <Card className={classes.wrapper}>
+      <CardContent style={{ padding: '15px 20px' }}>
+        <h3 style={{ textAlign: 'center' }}>Methodology and Data</h3>
         <Divider/>
         <h4>Quality Scores</h4>
         <p>
@@ -36,7 +43,7 @@ const Methodology = props => {
           Specific checks used to develop the ranking include the following:</p>
 
         <Grid container className="cardGrid">
-          <Grid item md={3} sm={12} xs={12} className="gridItem">
+          <Grid item md={3} sm={6} xs={12} className="gridItem">
             <Card className={classes.root}>
               <CardContent style={{ padding: 0 }}>
                 <h5>Road Connection Checks</h5>
@@ -52,7 +59,7 @@ const Methodology = props => {
               </CardContent>
             </Card>
           </Grid>
-          <Grid item md={3} sm={12} xs={12} className="gridItem">
+          <Grid item md={3} sm={6} xs={12} className="gridItem">
             <Card className={classes.root}>
               <CardContent style={{ padding: 0 }}>
                 <h5>Road Relation Checks</h5>
@@ -64,7 +71,7 @@ const Methodology = props => {
             </Card>
           </Grid>
 
-          <Grid item md={3} sm={12} xs={12} className="gridItem">
+          <Grid item md={3} sm={6} xs={12} className="gridItem">
             <Card className={classes.root}>
               <CardContent style={{ padding: 0 }}>
                 <h5>Road Tag Checks</h5>
@@ -80,7 +87,7 @@ const Methodology = props => {
               </CardContent>
             </Card>
           </Grid>
-          <Grid item md={3} sm={12} xs={12} className="gridItem">
+          <Grid item md={3} sm={6} xs={12} className="gridItem">
             <Card className={classes.root}>
               <CardContent style={{ padding: 0 }}>
                 <h5>Roundabout Checks</h5>
@@ -96,16 +103,14 @@ const Methodology = props => {
           </Grid>
         </Grid>
 
-        <p>These checks can be downloaded for use from the OSMLab
-          <a
+        <p>These checks can be downloaded for use from the OSMLab <a
             href="https://github.com/osmlab/atlas-checks/tree/dev/src/main/java/org/openstreetmap/atlas/checks/validation"
             target="_blank" rel="noopener noreferrer">Atlas-Checks</a> repository.</p>
 
         <h4>City Selection</h4>
         <p>Cities were chosen based on whether they were the capital city of a state or whether they were the most
-          populous city within a state.
-          <a href="https://en.wikipedia.org/wiki/List_of_largest_cities_of_U.S._states_and_territories_by_population"
-             target="_blank" rel="noopener noreferrer">Wikipedia</a> was a used to identify the most populous city in each state.</p>
+          populous city within a state. <a href="https://en.wikipedia.org/wiki/List_of_largest_cities_of_U.S._states_and_territories_by_population"
+             target="_blank" rel="noopener noreferrer">Wikipedia</a> was used to identify the most populous city in each state.</p>
 
         <h4>Grid Generation</h4>
         <p>Analysis grids were generated using a custom python tool which optimizes grid size based on map error
@@ -114,13 +119,21 @@ const Methodology = props => {
           critical mass of errors that represents a reasonable task for an editor. The resulting grid is thematically
           styled as a choropleth heat map and used by editors to prioritize their work. For each grid cell, the
           following statistics are summarized:</p>
-        <List>
-          <ListItem>grid size in km<sup>2</sup></ListItem>
-          <ListItem>% of road segments within the cell with map error</ListItem>
-          <ListItem>count of OSM features with the cell area (km<sup>2</sup>)</ListItem>
-          <ListItem>count of error flags within the cell</ListItem>
-          <ListItem>count of checks detecting error within the cell</ListItem>
-        </List>
+        <Grid item xs={12} className="gridItem">
+          <Card className={classes.root}>
+            <CardContent style={{ padding: 0 }}>
+              <h5>Statistics</h5>
+              <Divider/>
+              <List>
+                <ListItem>grid size in km<sup>2</sup></ListItem>
+                <ListItem>% of road segments within the cell with map error</ListItem>
+                <ListItem>count of OSM features with the cell area (km <sup>2</sup>)</ListItem>
+                <ListItem>count of error flags within the cell</ListItem>
+                <ListItem>count of checks detecting error within the cell</ListItem>
+              </List>
+            </CardContent>
+          </Card>
+        </Grid>
       </CardContent>
     </Card>
   );
