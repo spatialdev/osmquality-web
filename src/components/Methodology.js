417b0e3fee831be06cc43cfd30c1379a8760c4c0
Muning Brandeis <>
2019-08-15 21:07:41 -0700

    first_methodology_updates

--- src/components/Methodology.js
+++ src/components/Methodology.js
@@ -37,7 +37,7 @@ const Methodology = props => {
           <Divider/>
           <h4>Map Quality Evaluation</h4>
           <p>
-              Map quality is evaluated by looking at the OSM features (Ways, Nodes, and Relations) detected (flagged) by Atlas Checks. 
+              Map quality is evaluated by looking at the OSM features (Ways, Tags, and Relations) detected (flagged) by Atlas Checks. 
               Built on top of Atlas, a scalable OSM graph network, Atlas Checks allows users to write quality assurance algorithms 
               to find map errors in OSM data.
           </p>
@@ -46,59 +46,35 @@ const Methodology = props => {
           </p>
 
           <Grid container className="cardGrid">
-            <Grid item md={3} sm={6} xs={12} className="gridItem">
+            <Grid item md={6} sm={12} xs={24} className="gridItem">
               <Card className={classes.root}>
                 <CardContent style={{ padding: 0 }}>
-                  <h5>Road Connections</h5>
+                  <h5>Road Geometry Checks (6 checks)</h5>
                   <Divider/>
                   <List>
                     <ListItem>BuildingRoadIntersectionCheck</ListItem>
-                    <ListItem>DuplicateWaysCheck</ListItem>
+                    <ListItem>OverlappingEdgeCheck</ListItem>
                     <ListItem>EdgeCrossingEdgeCheck</ListItem>
-                    <ListItem>FloatingEdgeCheck</ListItem>
-                    <ListItem>SharpAngleCheck</ListItem>
-                    <ListItem>SinkIslandCheck</ListItem>
+                    <ListItem>SnakeRoadCheck</ListItem>
+                    <ListItem>RoundaboutValenceCheck</ListItem>
+                    <ListItem>InvalidMiniRoundaboutCheck</ListItem>
                   </List>
                 </CardContent>
               </Card>
             </Grid>
-            <Grid item md={3} sm={6} xs={12} className="gridItem">
+            <Grid item md={6} sm={12} xs={24} className="gridItem">
               <Card className={classes.root}>
                 <CardContent style={{ padding: 0 }}>
-                  <h5>Road Relations</h5>
+                  <h5>Road Tags and Relations Checks (7 Checks)</h5>
                   <Divider/>
                   <List>
                     <ListItem>InvalidTurnRestrictionCheck</ListItem>
-                  </List>
-                </CardContent>
-              </Card>
-            </Grid>
-
-            <Grid item md={3} sm={6} xs={12} className="gridItem">
-              <Card className={classes.root}>
-                <CardContent style={{ padding: 0 }}>
-                  <h5>Road Tags</h5>
-                  <Divider/>
-                  <List>
+                    <ListItem>HighwayToFerryTagCheck</ListItem>
                     <ListItem>InvalidAccessTagCheck</ListItem>
-                    <ListItem>SignPostCheck</ListItem>
-                    <ListItem>SnakeRoadCheck</ListItem>
-                    <ListItem>StreetNameIntegersOnlyCheck</ListItem>
+                    <ListItem>InvalidLanesTagCheck</ListItem>
+                    <ListItem>SignPostcheck</ListItem>
                     <ListItem>UnusualLayerTagCheck</ListItem>
-                    <ListItem>AreaWithHighwayTagCheck</ListItem>
-                  </List>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item md={3} sm={6} xs={12} className="gridItem">
-              <Card className={classes.root}>
-                <CardContent style={{ padding: 0 }}>
-                  <h5>Roundabouts</h5>
-                  <Divider/>
-                  <List>
-                    <ListItem>MalformedRoundaboutCheck</ListItem>
-                    <ListItem>RoundaboutClosedLoopCheck</ListItem>
-                    <ListItem>RoundaboutValenceCheck</ListItem>
+                    <ListItem>StreetNameIntegerOnlyCheck</ListItem>
                   </List>
                 </CardContent>
               </Card>
@@ -107,58 +83,23 @@ const Methodology = props => {
 
           <p>These checks can be downloaded for at: <a
             href="https://github.com/osmlab/atlas-checks"
-            target="_blank" rel="noopener noreferrer">https://github.com/osmlab/atlas-checks</a></p>
+            target="_blank" rel="noopener noreferrer">https://github.com/osmlab/atlas-checks</a> version 5.1.3</p>
 
           <h4>City Selection</h4>
-          <p>The most populous city in each state is selected. <a
-              href="https://en.wikipedia.org/wiki/List_of_largest_cities_of_U.S._states_and_territories_by_population"
-              target="_blank" rel="noopener noreferrer">Wikipedia</a> was used to identify those cities.</p>
+          <p>The most populous city in each state is selected. <a href="https://en.wikipedia.org/wiki/List_of_largest_cities_of_U.S._states_and_territories_by_population" target="_blank" rel="noopener noreferrer">Wikipedia</a> was used to identify those cities.</p>
 
           <h4>City Ranking</h4>
-          <p>Cities are ranked based on the map error rate which is calculated as below.</p>
-          <p>For atlas features within the subject area:</p>
-          <img className="equation" src={require('../images/formula_simple.png')} alt="equation for calculating city ranking" />
-          <p>The lower error rate a city has, the higher it is ranked. The error rate is normalized so that more weights are 
-            assigned to atlas features that are flagged multiple times.</p>
+          <p>Cities are ranked based on the map error rate with error OSM features over all OSM features. The lower error rate a city has, the higher it is ranked.</p>
 
           <h4>Data Processing</h4>
-          <p>OSM data used in analyses were downloaded from <a href="https://extract.bbbike.org/" target="_blank" rel="noopener noreferrer">BBBike</a> September 2018. Processing time for a given city region including data extraction, atlas-check processing and grid generation
-            ran about 30 minutes for this exercise. When machine environment settings are optimized for data processing,
-            the time required for all 51 cities to run in batch mode is approximately 24 hours.</p>
+          <p>TBD-Filled by Bentley</p>
 
           <h4>Grid Generation and Statistics</h4>
           <p>Analysis grids were generated using a custom python tool which optimizes grid size based on map error density. 
             The tool automates a process to find the optimal heat-map grid size, an area containing a critical mass of errors that represents 
             a reasonable task for an editor. The resulting grids are thematically styled as choropleth heat maps that can be used by editors 
-            to prioritize their work. For each grid cell, the following statistics are generated:</p>
-          <Grid item xs={12} className="gridItem">
-            <Card className={classes.root}>
-              <CardContent style={{ padding: 0 }}>
-                <List>
-                  <ListItem>Count of flagged Atlas features</ListItem>
-                  <ListItem>Count of flagged OSM features</ListItem>
-                  <ListItem>Count of flags</ListItem>
-                  <ListItem>Grid cell size</ListItem>
-                  <ListItem>Count of checks by type</ListItem>
-                </List>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <p>Afterwards, the grid-based statistics are used to generate the following statistics shown in the 'Quick Stats' section for each city:</p>
-
-          <Grid item xs={12} className="gridItem">
-            <Card className={classes.root}>
-              <CardContent style={{ padding: 0 }}>
-                <List>
-                  <ListItem>Count of total flagged OSM features (called OSM features)</ListItem>
-                  <ListItem>Count of total Atlas Checks flags (called Atlas Checks Flags)</ListItem>
-                  <ListItem>Total city area that are analyzed (called Total City Area)</ListItem>
-                  <ListItem>Percentages of checks by type</ListItem>
-                </List>
-              </CardContent>
-            </Card>
-          </Grid>
+            to prioritize their work. For each grid cell, we have counts on flagged OSM features and the size</p>
+  
         </div>
       </CardContent>
     </Card>
