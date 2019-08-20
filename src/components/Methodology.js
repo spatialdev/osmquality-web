004547559d8fccb364eb220dd294d7fd87a9e3b9
Muning Brandeis <>
2019-08-20 13:42:47 -0700

    add data processing section

--- src/components/Methodology.js
+++ src/components/Methodology.js
@@ -89,8 +89,17 @@ const Methodology = props => {
           <h4>City Ranking</h4>
           <p>Cities are ranked based on the map error rate with error OSM features over all OSM features. The lower error rate a city has, the higher it is ranked.</p>
 
-          <h4>Data Processing</h4>
-          <p>TBD-Filled by Bentley</p>
+          <h4>OSM PBF Extraction</h4>
+          <p> The OSM data used was extracted from a full OSM history PBF file (<a href="https://planet.openstreetmap.org/">Source</a>). The goal of the initial phase of data processing was to have two extracts for each city, one containing 2018 data and the other 2019 data. This was achieved using a tool called <a href="https://osmcode.org/osmium-tool/">Osmium</a>.</p>
+          <p> The first step was to cut the full planet OSM history PBF into a history file for each city using Osmium's 'extract' tool. The tool used geojson boundaries of each city to perform a soft clip for each, generating the new city history PBFs.</p>
+          <p>Next, each of the city history files were time sliced. Using Osmium's 'time-filter' tool each history file was filtered so that the data appeared the same as it would have at a specific point in time in OSM. For each city this was done twice, creating a PBF for April 7th at 10am UTC for both 2018 and 2019. At this point, the new time slice PBFs were in standard OSM PBF format, as opposed to OSM history files.</p>
+          <p>Finally, extraneous Nodes were cleaned from the time slice PBFs using a simple python script. The Nodes in question did not have an spatial information, as they were remnants of features that had been deleted at that point in time.</p>
+
+          <h4>MQM Extent Enhancement</h4>
+          <p>An addition to MQM was incorporated into our methodology this year, to better compare differences over time. The additional feature allows the extent of MQM results to be locked to a specified bounding box, as opposed to being dynamically set by the extent of the input data. This allowed the results of two MQM runs to share the same extent despite small variances in the spatial bound of the input data.</p>
+
+
+         
 
           <h4>Grid Generation and Statistics</h4>
           <p>Analysis grids were generated using a custom python tool which optimizes grid size based on map error density. 
