1c9949bc5260fc79776d2e05584437789b866abe
Micah Nacht <mjnacht@gmail.com>
2019-08-26 16:51:10 -0700

    update legend

--- src/components/MapLegend.js
+++ src/components/MapLegend.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const MapLegend = () => {
-  return (<div className='horizontalLegend'>
-    <div className='horizontalLegend-scale'>
-      <p>Map Errors</p>
-      <ul className='horizontalLegend-labels'>
-        <li><span style={{ background: '#ffffb2', opacity: 0.62 }}/>Low</li>
-        <li><span style={{ background: '#fd8d3c', opacity: 0.62 }}/>Medium</li>
-        <li><span style={{ background: '#bd0026', opacity: 0.62 }}/>High</li>
-      </ul>
-    </div>
-  </div>);
+  return <svg width="400" height="45">
+    <defs>
+      <linearGradient id="gradient" x1="0%" y1="100%" x2="100%" y2="100%" spreadMethod="pad">
+        <stop offset="0%" stopColor="#f3ebed" stopOpacity="1"/>
+        <stop offset="50%" stopColor="#c2a0c0" stopOpacity="1"/>
+        <stop offset="100%" stopColor="#5c3366" stopOpacity="1"/>
+      </linearGradient>
+    </defs>
+    <rect width="200" height="15" transform="translate(0,0)" style={{fill: "url(\"#gradient\")"}}/>
+    <g className="y axis" transform="translate(0,30)">
+      <text y="-2" x="0">Low</text>
+    </g>
+    <g className="y axis" transform="translate(0,30)">
+      <text y="-2" x="174">High</text>
+    </g>
+  </svg>
 };
 
 export default MapLegend;
