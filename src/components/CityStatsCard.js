ebc20879a1b52c000cf6930a51a3c504cf91488d
sayana-saithu <sayana_saithu@apple.com>
2019-08-28 11:03:50 -0700

    Code clean up

--- src/components/CityStatsCard.js
+++ src/components/CityStatsCard.js
@@ -2,12 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
 
 import '../App.css';
 import { Chart } from "react-google-charts";
-import PieChartLegend from '../components/PieChartLegend';
 
 import numberWithCommas from '../helpers/helpers';
 
@@ -17,7 +14,6 @@ import grid_icon from '../images/grid_icon.svg';
 import trending_flat from '../images/trending_flat.svg';
 import trending_down from '../images/trending_down.svg';
 import trending_up from '../images/trending_up.svg';
-import {TrendingUp, TrendingDown, TrendingFlatOutlined, DragHandle} from "@material-ui/icons";
 
 const CityStatsCard = props => {
   const { data } = props;
@@ -27,73 +23,58 @@ const CityStatsCard = props => {
       margin: '10px'
     }}>
       <CardContent style={{ padding: 0 }}>
-        <h3 className="cardHeader">
-          Quick Stats
-        </h3>
-        <div className="gridRoot">
-            <GridList cellHeight={160} className="gridList" cols={2}>
-                <GridListTile key={data.features} cols={1} style={{ height: '100px' }} classes={{
-                    tile: 'gridTileTile',
-                    root: 'gridTileRoot'
-                }}>
-                    <div className="statDiv">
-                        <div>
-                            <object data={ flagTrend > 0 ? trending_up: flagTrend<0 ? trending_down : trending_flat} className={flagTrend > 0 ? "trendingUp":
-                                flagTrend<0 ? "trendingDown" : "trendingFlat"}>Flag Icon</object>
-                            <div>
-                                <h2 style={{ margin: 0, display: 'inline' }}>{flagTrend.toFixed(2)}%</h2>
-                                <p>Quality Trend (vs. 2018)</p>
-                            </div>
-                        </div>
+          <div className="cardHeaderContainer">
+              <h3 className="cardHeader">
+                  Quick Stats
+              </h3>
+          </div>
+        <div >
+            <div className="statDiv">
+                <div>
+                    <object data={flagTrend > 0 ? trending_up : flagTrend < 0 ? trending_down : trending_flat}
+                            className={flagTrend > 0 ? "trendingUp" :
+                                flagTrend < 0 ? "trendingDown" : "trendingFlat"}>Flag Icon
+                    </object>
+                    <div>
+                        <h2 style={{margin: 0, display: 'inline'}}>{flagTrend.toFixed(2)}%</h2>
+                        <p>Quality Trend (vs. 2018)</p>
                     </div>
-                </GridListTile>
-                <GridListTile key={data.flags} cols={1} style={{ height: '100px' }} classes={{
-                    tile: 'gridTileTile',
-                    root: 'gridTileRoot'
-                }}>
-                    <div className="statDiv">
-                        <div>
-                            <object data={flag_icon} className="statsIcon">Flag Icon</object>
-                            <div>
-                                <h2 style={{ margin: 0, display: 'inline' }}>{numberWithCommas(data.flags)}</h2>
-                                <p>Affected OSM features</p>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div className="statDiv">
+                <div>
+                    <object data={flag_icon} className="statsIcon">Flag Icon</object>
+                    <div>
+                        <h2 style={{margin: 0, display: 'inline'}}>{numberWithCommas(data.flags)}</h2>
+                        <p>Affected OSM features</p>
                     </div>
-                </GridListTile>
-                <GridListTile key={data.totalRoads} cols={1} style={{ height: '100px' }} classes={{
-                    tile: 'gridTileTile',
-                    root: 'gridTileRoot'
-                }}>
-                    <div className="statDiv">
-                        <div>
-                            <object data={shape_icon} className="statsIcon">Shape Icon</object>
-                            <div>
-                                <h2 style={{ margin: 0, display: 'inline' }}>{numberWithCommas(data.totalRoads.toFixed(0))}</h2>
-                                <p>Total OSM Road Features </p>
-                            </div>
-                        </div>
+                </div>
+            </div>
+
+            <div className="statDiv">
+                <div>
+                    <object data={shape_icon} className="statsIcon">Shape Icon</object>
+                    <div>
+                        <h2 style={{margin: 0, display: 'inline'}}>{numberWithCommas(data.totalRoads.toFixed(0))}</h2>
+                        <p>Total OSM Road Features </p>
                     </div>
-                </GridListTile>
-                <GridListTile key={data.gridSize} cols={1} style={{ height: '100px' }} classes={{
-                    tile: 'gridTileTile',
-                    root: 'gridTileRoot'
-                }}>
-                    <div className="statDiv">
-                        <div>
-                            <object data={grid_icon} className="statsIcon">Grid Icon</object>
-                            <div>
-                                <h2 style={{ margin: 0, display: 'inline' }}>{numberWithCommas(data.gridSize.toFixed(1))}</h2>
-                                <p>Grid Cell Size (km<sup>2</sup>)</p>
-                            </div>
-                        </div>
+                </div>
+            </div>
+
+            <div className="statDiv">
+                <div>
+                    <object data={grid_icon} className="statsIcon">Grid Icon</object>
+                    <div>
+                        <h2 style={{margin: 0, display: 'inline'}}>{numberWithCommas(data.gridSize.toFixed(1))}</h2>
+                        <p>Grid Cell Size (km<sup>2</sup>)</p>
                     </div>
-                </GridListTile>
-            </GridList>
+                </div>
+            </div>
             <div>
-                <div className ='chart-title'>Map Errors by Type</div>
+                <div className ='chartTitle'>Map Errors by Type</div>
 
                 <Chart
+                    className="barChart"
                     chartType="BarChart"
                     loader={<div>Loading Chart</div>}
                     data={[
@@ -112,10 +93,9 @@ const CityStatsCard = props => {
                         ['Road Attributes', data.roadAttributes, '#129BB1', null],
                     ]}
                     options={{
-
-                        width: 400,
-                        height: 150,
-                        bar: { groupWidth: '40%' },
+                        width: 300,
+                        height: 120,
+                        bar: { groupWidth: '50%' },
                         legend: { position: 'none' },
 
                     }}
