530aaba1868d32340b8a5862de3c030cc011b782
sayana-saithu <saithusayana@gmail.com>
2021-07-08 15:44:42 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.448a8e1635390cca803401d4ab689782.js"
+  "/precache-manifest.a7ee061d47c74b0867b829966405e449.js"
 );
 
 workbox.clientsClaim();
